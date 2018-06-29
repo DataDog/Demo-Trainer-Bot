aa9e127dbf6b8745a8fca00a6101fb07342879d9
james.leung <LeungJ.417@gmail.com>
2018-06-29 14:24:51 -0400

    Moved fixture injection to utils

--- tests/test_calendar_service.py
+++ tests/test_calendar_service.py
@@ -1,21 +1,8 @@
-import pytest
 import os
-from functools import partial
+import pytest
+
+from utils.test_inject import auto_inject_fixtures
 from expects import expect, equal, be_above
-
-
-def _inject(cls, names):
-    @pytest.fixture(autouse=True)
-    def auto_injector_fixture(self, request):
-        for name in names:
-            setattr(self, name, request.getfixturevalue(name))
-
-    cls.__auto_injector_fixture = auto_injector_fixture
-    return cls
-
-
-def auto_inject_fixtures(*names):
-    return partial(_inject, names=names)
 
 
 @auto_inject_fixtures('google_calendar')
@@ -35,15 +22,13 @@ class TestCalendar:
             'start':   {'dateTime': '2024-05-28T19:00:00%s' % GMT_OFF},
             'end':     {'dateTime': '2025-05-28T22:00:00%s' % GMT_OFF},
         }
-        result = self.google_calendar.create_event(evt, calendarId=SHARED_CALENDAR_ID)
+        result = self.google_calendar.create_event(
+            evt, calendarId=SHARED_CALENDAR_ID)
 
         expect(str(result['status'])).to(equal('confirmed'))
         expect(str(result['summary'])).to(equal('Dinner with friends'))
         expect(str(result['kind'])).to(equal('calendar#event'))
 
-        del_result = self.google_calendar.delete_event(result['id'], calendarId=SHARED_CALENDAR_ID)
+        del_result = self.google_calendar.delete_event(
+            result['id'], calendarId=SHARED_CALENDAR_ID)
         expect(del_result).to(equal(''))
-
-        
-
-
